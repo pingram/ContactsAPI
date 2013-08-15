d1646310ade884bc581c7ab7c8e8893f73072259
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-15 01:58:22 -0700

    add other UsersController actions; do routes exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+  end
+
   def index
     render :json => User.all
+  end
+
+  def show
+    render :json => User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(params[:user])
+      render :json => user
+    else
+      render :json => user.errors, :status => :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 FirstRoutes::Application.routes.draw do
   resources :users
+  
+  # get "/users", :to => "users#index"
+  # post "/users", :to => "users#create"
+  # get "/users/new", :to => "users#new"
+  # get "/users/:id/edit", :to => "users#edit"
+  # get "/users/:id", :to => "users#show"
+  # put "/users/:id", :to => "users#update"
+  # delete "/users/:id", :to => "users#destroy"
 end
