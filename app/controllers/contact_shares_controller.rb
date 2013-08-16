c0ce7786b6e580942ad8ca0445afdee959d05ffc
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-15 23:40:52 -0700

    fix ContactSharesController actions

--- app/controllers/contact_shares_controller.rb
+++ app/controllers/contact_shares_controller.rb
@@ -1,9 +1,11 @@
 class ContactSharesController < ApplicationController
   def create
     ContactShare.create(params[:contact_share])
+    head :ok
   end
 
   def destroy
     ContactShare.find(params[:id]).destroy
+    head :ok
   end
 end
