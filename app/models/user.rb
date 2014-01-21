8d8e3cf6a11d28b715ad412a6d0b2c47fc14ec21
Sid Raval <sidsraval@gmail.com>
2014-01-21 13:53:01 -0500

    Adds source to through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :contacts
   has_many :contact_shares
-  has_many :shared_contacts, :through => :contact_shares
+  has_many :shared_contacts, :through => :contact_shares, source: :contact
 
   validates :username, :presence => true
   validates :username, :uniqueness => true
